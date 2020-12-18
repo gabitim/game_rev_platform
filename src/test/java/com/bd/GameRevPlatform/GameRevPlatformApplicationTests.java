7f5fc80e95feb96fa535bf5a4209acdda4eb55f6
Timofti Gabriel <gabitim470@gmail.com>
2020-12-19 01:28:00 +0200

    FEAT: project created

---
+++ src/test/java/com/bd/GameRevPlatform/GameRevPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.GameRevPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRevPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
