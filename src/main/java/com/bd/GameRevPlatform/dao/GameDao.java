8697fc08fce76af6c68839929ca7176cddb19713
Timofti Gabriel <gabitim470@gmail.com>
2020-12-20 01:40:48 +0200

    FEAT: GameDao Class insert a Game

--- src/main/java/com/bd/GameRevPlatform/dao/GameDao.java
+++ src/main/java/com/bd/GameRevPlatform/dao/GameDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Types;
 import java.util.List;
 
 /**
@@ -26,7 +27,19 @@ public class GameDao {
     }
 
     public void insertGame(Game game) {
+        String insertSql = "INSERT INTO Game (game_id, title, description, release_date, rating, news_column) VALUES (?, ?, ?, ?, ?, ?)";
+        Object[] args = new Object[] {
+                game.getGame_id(),
+                game.getTitle(),
+                game.getDescription(),
+                game.getRelease_date(),
+                game.getRating(),
+                game.getNews_column()
+        };
 
+        int[] types = new int[] {Types.NULL, Types.VARCHAR, Types.VARCHAR, Types.DATE, Types.NUMERIC, Types.VARCHAR};
+
+        jdbcTemplate.update(insertSql, args, types);
     }
 
     public Game getGame(int game_id) {
