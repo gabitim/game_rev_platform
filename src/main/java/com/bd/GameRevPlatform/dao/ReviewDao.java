dcb3cf498b20d2fdc792ef0ee7c5da70767cc7c5
Timofti Gabriel <gabitim470@gmail.com>
2020-12-27 22:14:57 +0200

    FEAT: ReviewDao add

--- src/main/java/com/bd/GameRevPlatform/dao/ReviewDao.java
+++ src/main/java/com/bd/GameRevPlatform/dao/ReviewDao.java
@@ -1,7 +1,11 @@
 package com.bd.GameRevPlatform.dao;
 
+import com.bd.GameRevPlatform.model.Review;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Timofti Gabriel
@@ -13,6 +17,19 @@ public class ReviewDao {
 
     public ReviewDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Review> getAllReviews() {
+        String sql = "SELECT * FROM Review";
+
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Review.class));
+    }
+
+    public Review getReview(int review_id){
+        String sql = "SELECT * FROM Review WHERE review_id = ?";
+        Object[] args = {review_id};
+
+        return jdbcTemplate.queryForObject(sql, args, BeanPropertyRowMapper.newInstance(Review.class));
     }
 
     public void deleteReview(int game_id) {
---
+++ src/main/java/com/bd/GameRevPlatform/model/Review.java
@@ -0,0 +1,71 @@
+package com.bd.GameRevPlatform.model;
+
+/**
+ * @author Timofti Gabriel
+ */
+public class Review {
+    private int review_id;
+    private String title;
+    private String text_field;
+    private int game_id;
+    private int user_id;
+
+    public Review(int review_id, String title, String text_field, int game_id, int user_id) {
+        this.review_id = review_id;
+        this.title = title;
+        this.text_field = text_field;
+        this.game_id = game_id;
+        this.user_id = user_id;
+    }
+
+    public int getReview_id() {
+        return review_id;
+    }
+
+    public void setReview_id(int review_id) {
+        this.review_id = review_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText_field() {
+        return text_field;
+    }
+
+    public void setText_field(String text_field) {
+        this.text_field = text_field;
+    }
+
+    public int getGame_id() {
+        return game_id;
+    }
+
+    public void setGame_id(int game_id) {
+        this.game_id = game_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "review_id=" + review_id +
+                ", title='" + title + '\'' +
+                ", text_field='" + text_field + '\'' +
+                ", game_id=" + game_id +
+                ", user_id=" + user_id +
+                '}';
+    }
+}
