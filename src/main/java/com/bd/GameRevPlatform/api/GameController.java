4822a35ddfe12885d20c2ca80f57f7f79f87be59
Timofti Gabriel <gabitim470@gmail.com>
2021-01-06 18:14:12 +0200

    REFACTOR: profile avatar link integrated in new, edit, delete game

--- src/main/java/com/bd/GameRevPlatform/api/GameController.java
+++ src/main/java/com/bd/GameRevPlatform/api/GameController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.text.ParseException;
 import java.util.List;
@@ -47,58 +48,75 @@ public class GameController {
         return "index";
     }
 
-    @RequestMapping("/newGame")
-    public String viewEnterGamePage(Model model) {
+    @RequestMapping("/{user_id}/newGame")
+    public String viewEnterGamePage(Model model, @PathVariable(name = "user_id")int user_id) {
         FrontPageGame game = new FrontPageGame();
         List<Genre> genres = genreService.getAllGenres();
+        Userr userr = userrService.getUserById(user_id);
 
         model.addAttribute("game", game);
         model.addAttribute("genres", genres);
+        model.addAttribute("user", userr);
 
         return "new_game_form";
     }
 
-    @RequestMapping(value = "/saveGame", method = RequestMethod.POST)
-    public String saveGame(@ModelAttribute("game") FrontPageGame game) {
+    @RequestMapping(value = "/{user_id}/saveGame", method = RequestMethod.POST)
+    public String saveGame(@ModelAttribute("game") FrontPageGame game,
+                           @PathVariable(name = "user_id")int user_id,
+                           RedirectAttributes redirectAttributes) {
         gameService.saveGame(game);
 
-        return "redirect:/main_page";
+        redirectAttributes.addAttribute("user_id", user_id);
+        return "redirect:/main_page/{user_id}";
     }
 
-    @RequestMapping("/edit/{game_id}")
-    public ModelAndView viewEditGamePage(@PathVariable(name = "game_id")int game_id ) {
+    @RequestMapping("/{user_id}/edit/{game_id}")
+    public ModelAndView viewEditGamePage(@PathVariable(name = "game_id")int game_id,
+                                         @PathVariable(name = "user_id")int user_id) {
         ModelAndView modelAndView = new ModelAndView("edit_game_form");
         FrontPageGame frontPageGame = gameService.getGameFrontPage(game_id);
         List<Genre> genres = genreService.getAllGenres();
+        Userr userr = userrService.getUserById(user_id);
 
         modelAndView.addObject("game", frontPageGame);
         modelAndView.addObject("genres", genres);
+        modelAndView.addObject("user", userr);
 
         return modelAndView;
     }
 
-    @RequestMapping(value = "/updateGame", method = RequestMethod.POST)
-    public String updateGame(@ModelAttribute("game") FrontPageGame game) {
+    @RequestMapping(value = "/{user_id}/updateGame", method = RequestMethod.POST)
+    public String updateGame(@ModelAttribute("game") FrontPageGame game,
+                             @PathVariable(name = "user_id")int user_id,
+                             RedirectAttributes redirectAttributes) {
         gameService.updateGame(game);
 
-        return "redirect:/main_page";
+        redirectAttributes.addAttribute("user_id", user_id);
+        return "redirect:/main_page/{user_id}";
     }
 
-    @RequestMapping("/delete/{game_id}")
-    public String deleteGame(@PathVariable(name = "game_id")int game_id ) {
+    @RequestMapping("/{user_id}/delete/{game_id}")
+    public String deleteGame(@PathVariable(name = "game_id")int game_id,
+                             @PathVariable(name = "user_id")int user_id,
+                             RedirectAttributes redirectAttributes) {
         gameService.deleteGame(game_id);
 
-        return "redirect:/main_page";
+        redirectAttributes.addAttribute("user_id", user_id);
+        return "redirect:/main_page/{user_id}";
     }
 
-    @RequestMapping("/game/{game_id}")
-    public ModelAndView viewGame(@PathVariable(name = "game_id")int game_id) {
+    @RequestMapping("/{user_id}/game/{game_id}")
+    public ModelAndView viewGame(@PathVariable(name = "game_id")int game_id,
+                                 @PathVariable(name = "user_id")int user_id) {
         ModelAndView modelAndView = new ModelAndView("game_form");
         List<Review> reviews = reviewService.getReviewsByGameId(game_id);
         FrontPageGame frontPageGame = gameService.getGameFrontPage(game_id); //for displaying game title
+        Userr userr = userrService.getUserById(user_id);
 
         modelAndView.addObject("reviews", reviews);
         modelAndView.addObject("game", frontPageGame);
+        modelAndView.addObject("user", userr);
 
         return modelAndView;
     }
